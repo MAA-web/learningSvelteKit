67ccf3977c592cee085033b78d6d5fbfa458b08a
MAA-web <64776926+MAA-web@users.noreply.github.com>
2023-01-07 22:17:40 +0000

    just adding some buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<div class="top">
+<button>hi</button>
+<button>hey</button>
+<button>hello</button>
+</div>
+<div class="bottom">
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button>
+    sup sup
+</button>
+</div>
